2781aab180454aaedf3d770d08621db14ba084b6
Luis Plata <www.luisplata@gmail.com>
2021-10-25 21:15:23 -0500

    First MVP

--- Assets/Scripts/View/InputController.cs
+++ Assets/Scripts/View/InputController.cs
@@ -7,15 +7,29 @@ namespace View
         [SerializeField] private GameObject point2;
 
         [SerializeField] private Tank tank;
+        [SerializeField] private MenuController menu;
+
+        private Camera _camera;
+
         // Update is called once per frame
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
         void Update()
         {
             if (Input.GetMouseButton(0))
             {
-                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector3 worldPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
                 worldPosition.z = 0;
                 point2.transform.position = worldPosition;
                 Fire(worldPosition);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                menu.LoadScene(0);
             }
         }
 
