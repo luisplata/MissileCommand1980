99649a68a847fc1663eb45a6815c9794a6a523d9
Luis Plata <www.luisplata@gmail.com>
2021-10-26 10:00:02 -0500

    adding playfab

---
+++ Assets/Scripts/ServiceLocatorPath/Installer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace ServiceLocatorPath
+{
+    public class Installer : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var playFab = new PlayFabCustom();
+            ServiceLocator.Instance.RegisterService<IPlayFabCustom>(playFab);
+            if (GameObject.FindObjectsOfType<Installer>().Length > 1)
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
